a146dae6e61d1ec1de8d3d62fc6dc30982d9386e
Cherisia <yhcyoon@kakao.com>
2026-04-13 16:36:32 +0900

    echo 제거

--- run_capture.bat
+++ run_capture.bat
@@ -1,14 +1,5 @@
 @echo off
-chcp 65001 > nul
 cd /d "%~dp0"
-
-echo.
-echo ========================================
-echo   serenkit 캡처 도구
-echo ========================================
-echo.
-
 node capture.js
-
 echo.
 pause
